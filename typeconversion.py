81554474da9b58b4194c14f3b0415da96367f1fe
sanjay-aws-certified <prakashsanjay19@gmail.com>
2022-07-25 23:09:22 +0530

    second commit

---
+++ typeconversion.py
@@ -0,0 +1,6 @@
+# write a program that will ask the year that we were born &
+# it will calculate our age to print in the terminal
+birth_year = input('Birth year: ')
+age= 2022 - int(birth_year)
+print (age)
+
