0aab8ed36347e73a65814e9db2472b97567e47ad
sanjay-aws-certified <prakashsanjay19@gmail.com>
2022-07-26 19:11:15 +0530

    commit if/else

---
+++ if.py
@@ -0,0 +1,19 @@
+from operator import truediv
+
+is_hotday= True
+is_coldday= False
+
+if is_hotday:
+
+      print("It's a hot day")
+      print ("Drink plenty of Water")
+
+elif is_coldday:
+
+      print ("It's Cold day")
+      print("Wear Warm Clothes") 
+
+else:
+    print ("It's a lovely day") #if both the condition is false execute this method
+     
+         
