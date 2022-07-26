18e1a39801227d6e4b920e8e43e5ffb53a35977e
sanjay-aws-certified <prakashsanjay19@gmail.com>
2022-07-26 08:56:05 +0530

    new python file

---
+++ weight.py
@@ -0,0 +1,12 @@
+from unittest import result
+
+
+# write a simple program in python which can convert weight in pound into kg
+
+weight_lbs=input("weight in pound : ")
+weight_kg= 0.45
+result= (float (weight_lbs)* weight_kg)
+print ('weight in kg: ', result)
+
+
+
