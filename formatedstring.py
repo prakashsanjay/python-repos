f63f4abce7297f9cd5183c0823efa664071a7bfb
sanjay-aws-certified <prakashsanjay19@gmail.com>
2022-07-26 16:38:56 +0530

    formated string

---
+++ formatedstring.py
@@ -0,0 +1,9 @@
+from encodings import CodecRegistryError
+
+first_name='Utkarsh'
+last_name='Singh'
+msg = f'{first_name} [{last_name}] is a coder'
+print (msg)
+
+# to define formatted strings prefix your value with f & than use curly braces
+# to dynamically insert value to your strings.
