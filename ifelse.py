ea4685452fad564f197d15b2c9c4ce63a6008d30
sanjay-aws-certified <prakashsanjay19@gmail.com>
2022-07-26 20:23:43 +0530

    commit new if/else statement)

---
+++ ifelse.py
@@ -0,0 +1,14 @@
+price_house= 1000000
+good_credit= True
+bad_credit= False
+
+if good_credit:
+    down_payment = 0.1 * price_house
+    
+else:
+    down_payment = 0.2 * price_house
+
+print (f"Down Payment ${down_payment}")
+
+
+
