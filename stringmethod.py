dfac861d920027bb0b17bfc2827e4e0e70a96b73
sanjay-aws-certified <prakashsanjay19@gmail.com>
2022-07-26 18:25:48 +0530

    string method

---
+++ stringmethod.py
@@ -0,0 +1,10 @@
+course = 'Python For Beginners'
+print (len(course))    # to count the no of characters in the string
+print (course.upper()) # display the string in upper case
+print (course.lower()) # display the string in lower case
+print (course)         # display the original string
+print (course.find('F')) # display the index of letter F in the string
+print (course.replace('Beginners' ,'Absolute Beginners'))
+ # above method is to replace a character in the string
+print ('Python' in course) # display boolean value true/false
+# 'find' method produces a index value where as 'in' method produces a boolean value
